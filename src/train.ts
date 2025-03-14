0ce39063a906896166b59b4a09ea101bb4e362d6
dilshod-14 <urolovdilshod24@gmail.com>
2025-03-15 08:42:42 +0900

    feat: Tack-H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,6 @@
-console.log("helloWorld")
+function getPositiveString(list: number[]): string {
+    return list.filter((num: number) => num > 0).join("");
+}
+
+console.log(getPositiveString([5, -2, 12, -8, 21, -4]));
+ 
