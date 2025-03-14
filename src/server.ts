87f111b5231bf47fa7480c757581d6f5f353ccc3
dilshod-14 <urolovdilshod24@gmail.com>
2025-03-15 08:25:26 +0900

    feat:exoress integrations

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-
+import app from "./app";
 
 mongoose
-  .connect(process.env.MONGO_URL as string, {})
+  .connect(process.env.MONGO_URL as string, {}) 
   .then((data) => {
     console.log("MangoDB connction succeed");
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`Thi\e servsr is running succeesfully on port: ${PORT}`);
+    });
   })
-  .catch((err) => console.log("ERROR on connection MongoDB", err));        
-    
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
