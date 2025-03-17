43189777f4bd7af7d986f84493e0bfe7590b1da2
dilshod-14 <urolovdilshod24@gmail.com>
2025-03-18 07:08:17 +0900

    feat:MITTASK H2

--- src/train.ts
+++ src/train.ts
@@ -1,6 +1,14 @@
-function getPositiveString(list: number[]): string {
-    return list.filter((num: number) => num > 0).join("");
+// function getPositiveString(list: number[]): string {
+//     return list.filter((num: number) => num > 0).join("");
+// }
+
+// console.log(getPositiveString([5, -2, 12, -8, 21, -4]));
+
+function list(a: string): string {
+  return a
+    .split("")
+    .filter((b) => !isNaN(Number(b)) && b !== " ")
+    .join("");
 }
 
-console.log(getPositiveString([5, -2, 12, -8, 21, -4]));
- 
+console.log(list("m14il1iu"));
