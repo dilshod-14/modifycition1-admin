693a653f5e86ac864f8bcb630c97406aac7d021f
dilshod-14 <urolovdilshod24@gmail.com>
2025-05-02 21:28:12 +0900

    feat: develop getTopUsers rest Api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -11,6 +11,7 @@ import * as bcrypt from "bcryptjs";
 import { shapeIntoMongooseObjectId } from "../lips/config";
 
 class MemberService {
+  
   private readonly memberModel;
 
   constructor() {
@@ -75,7 +76,21 @@ class MemberService {
     const result = await this.memberModel
       .findOneAndUpdate({ _id: memberId }, input, { new: true })
       .exec();
-      if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATED_FAILED);
+    if (!result)
+      throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATED_FAILED);
+    return result;
+  }
+
+  public async getTopUsers(): Promise<Member[]> {
+    const result = await this.memberModel
+      .find({
+        memberStatus: MemberStatus.ACTIVE,
+        memberPoints: { $gte: 1 }
+      })
+      .sort({ memberPoints: -1 })
+      .limit(4)
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
     return result;
   }
 
--- src/router.ts
+++ src/router.ts
@@ -24,7 +24,7 @@ router.post(
 
   memberController.updateMember
 );
-
+router.get("/member/top-users", memberController.getTopUsers)
 export default router;
 /** Product */
 
