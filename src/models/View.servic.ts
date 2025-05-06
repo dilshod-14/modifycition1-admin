2d74f3c7bb904ca8f94e70d3aee4ea6922cb3ad0
dilshod-14 <urolovdilshod24@gmail.com>
2025-05-06 18:22:56 +0900

    feat: create view schima and service models

---
+++ src/models/View.servic.ts
@@ -0,0 +1,10 @@
+import ViewModel from "../schema/View.model";
+
+class ViewService {
+  private readonly ViewModel;
+  constructor() {
+    this.ViewModel = ViewModel;
+  }
+}
+
+export default ViewService;
