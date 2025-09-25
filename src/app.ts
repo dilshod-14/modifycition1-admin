cf0581ac259183d55fd0c367f5be9e92867e9230
dilshod-14 <urolovdilshod24@gmail.com>
2025-09-25 15:58:39 +0900

    fix: uploads papkani yaratdik

--- src/app.ts
+++ src/app.ts
@@ -6,37 +6,36 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
-import { T } from "./libs/types/common";
 
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
+import { T } from "./libs/types/common";
 
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
   uri: String(process.env.MONGO_URL),
-  collection: "sessions"
+  collection: "sessions",
 });
-/** 1-ENTRANCE**/
+// 1-ENTRANCE/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-
+app.use("/uploads", express.static("./uploads"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
-/** 2-SESSION**/
+// 2-SESSION/
 app.use(
   session({
-    secret: String(process.env.SESSION_SECRET),
+    secret: String(process.env.SESSION_SECRET), // 2ta vazifasi zashita & database codelar bn req codelarni tekshiradi
     cookie: {
-      maxAge: 1000 * 3600 * 6 // 3h
+      maxAge: 1000 * 3600 * 6, // 6h
     },
     store: store,
 
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true, // boshqalar ham kirsa session yaratib beradi true bo'lsa false bo'lsa faqat memberlar uchun
   })
 );
 app.use(function (req, res, next) {
@@ -44,11 +43,11 @@ app.use(function (req, res, next) {
   res.locals.member = sessionInstance.member;
   next();
 });
-
-/** 3-VIEW**/
+//3-VIEW/
 app.set("views", path.join(__dirname, "views"));
+// console.log("views", __dirname);
 app.set("view engine", "ejs");
-/** 4-ROUTERS**/
+//4-ROUTERS/;
 app.use("/admin", routerAdmin); //BSSR: EJS
 app.use("/", router); // SPA: REACT
 
--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,38 @@ dotenv.config();
 import mongoose from "mongoose";
 import app from "./app";
 
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT ?? 3003;
+
+if (!MONGO_URL) {
+  throw new Error("❌ MONGO_URL not defined in .env file");
+}
+
 mongoose
-  .connect(process.env.MONGO_URL as string, {})
+  .connect(MONGO_URL)
   .then(() => {
-    console.log("MongoDB connection succeed");
-
-    const PORT = parseInt(process.env.PORT || "3003", 10); // ✅ number ga o‘girildi
-
-    app.listen(PORT, "0.0.0.0", function () {
-      console.info(`Server is running successfully on port: ${PORT}`);
-      console.info(`Admin project on http://34.64.145.37:${PORT}/admin \n`);
+    console.log("✅ MongoDB connection succeed");
+    app.listen(PORT, () => {
+      console.info(`🚀 Server is running on http://localhost:${PORT}/admin\n`);
     });
   })
-  .catch((err) => console.log("ERROR on connection MongoDB", err));
+  .catch((err) => console.error("❌ ERROR on MongoDB connection:", err));
+
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+ 
+// console.log("PORT:", process.env.PORT);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
+
+//Architectural pattern: MVC, Dependency Injection, MVP
+
+//Design pattern: Middleware, Decotar
+
+// console.log("EXUCUTED!");
+
+// import moment from "moment";
+
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
+
+// const person: string = "MArtin";
+// const count: number = 100;
