6782272a0d725832d91d554d41f9bf0be942da57
dilshod-14 <urolovdilshod24@gmail.com>
2025-09-26 19:31:33 +0900

    feat: develop socket io server

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
+import { Socket } from "socket.io";
 
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
@@ -14,41 +17,69 @@ import { T } from "./libs/types/common";
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
   uri: String(process.env.MONGO_URL),
-  collection: "sessions",
+  collection: "sessions"
 });
-// 1-ENTRANCE/
+
+/** = ENTRANCE = **/
 const app = express();
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("./uploads"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ credentials: true, origin: true }));
+app.use(
+  cors({
+    credentials: true,
+    origin: true
+  })
+);
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
+
 // 2-SESSION/
 app.use(
   session({
-    secret: String(process.env.SESSION_SECRET), // 2ta vazifasi zashita & database codelar bn req codelarni tekshiradi
+    secret: String(process.env.SESSION_SECRET),
     cookie: {
-      maxAge: 1000 * 3600 * 6, // 6h
+      maxAge: 1000 * 3600 * 6 // 6h
     },
     store: store,
-
     resave: true,
-    saveUninitialized: true, // boshqalar ham kirsa session yaratib beradi true bo'lsa false bo'lsa faqat memberlar uchun
+    saveUninitialized: true
   })
 );
+
 app.use(function (req, res, next) {
   const sessionInstance = req.session as T;
   res.locals.member = sessionInstance.member;
   next();
 });
-//3-VIEW/
-app.set("views", path.join(__dirname, "views"));
-// console.log("views", __dirname);
-app.set("view engine", "ejs");
-//4-ROUTERS/;
-app.use("/admin", routerAdmin); //BSSR: EJS
-app.use("/", router); // SPA: REACT
 
-export default app;
+/** 3 - VIEWS **/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/** 4 - ROUTERS **/
+app.use("/admin", routerAdmin); // SSR
+app.use("/", router); // SPA
+
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: true,
+    credentials: true
+  }
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+    console.log(`disconnection & total [${summaryClient}]`);
+  });
+});
+
+export default server;
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
 import app from "./app";
 
-const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT ?? 3003;
-
-if (!MONGO_URL) {
-  throw new Error("❌ MONGO_URL not defined in .env file");
-}
-
 mongoose
-  .connect(MONGO_URL)
-  .then(() => {
-    console.log("✅ MongoDB connection succeed");
-    app.listen(PORT, () => {
-      console.info(`🚀 Server is running on http://localhost:${PORT}/admin\n`);
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.info(`The server is running successfully on port: ${PORT}`);
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`);
     });
   })
-  .catch((err) => console.error("❌ ERROR on MongoDB connection:", err));
-
-// CLUSTER => DATABASE => COLLECTION => DOCUMENT
- 
-// console.log("PORT:", process.env.PORT);
-// console.log("MONGO_URL:", process.env.MONGO_URL);
-
-//Architectural pattern: MVC, Dependency Injection, MVP
-
-//Design pattern: Middleware, Decotar
-
-// console.log("EXUCUTED!");
-
-// import moment from "moment";
-
-// const currentTime = moment().format("YYYY MM DD");
-// console.log(currentTime);
-
-// const person: string = "MArtin";
-// const count: number = 100;
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
